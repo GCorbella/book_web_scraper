1d716cc5fda01462aaf381afe292755ff2e663f8
Grifo55 <guillepcorbella@hotmail.com>
2022-06-27 16:16:42 +0200

    Script finished.

--- book_web_scraper.py
+++ book_web_scraper.py
@@ -1,7 +1,35 @@
 import requests
 import bs4
 
+# URL without page number
 base_url = "http://books.toscrape.com/catalogue/page-{}.html"
 
-for n in range(1,51):
-    print(base_url.format(n))
+# 4 - 5 rating books list
+high_r_books = []
+
+# Page iteration
+for page in range(1,51):
+
+    # Create soup for every page
+    page_url = base_url.format(page)
+    result = requests.get(page_url)
+    soup = bs4.BeautifulSoup(result.text, "lxml")
+
+    # Select book data
+    books = soup.select(".product_pod")
+
+    # Book iteration
+    for book in books:
+
+        # Check 4-5 Star rating
+        if len(book.select(".star-rating.Four")) != 0 or len(book.select(".star-rating.Five")) != 0:
+
+            # Save title
+            book_title = book.select("a")[1]["title"]
+
+            # Append book to list
+            high_r_books.append(book_title)
+
+# Show 4-5 star rating books
+for b in high_r_books:
+    print(b)
